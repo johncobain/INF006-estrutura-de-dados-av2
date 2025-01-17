b6f95ffb082063c80da00429672f0b8ceed901ac
Andrey Gomes <andreygdsn@gmail.com>
2025-01-16 22:04:14 -0300

    question 1 done

--- RL1Q1.c
+++ RL1Q1.c
@@ -9,18 +9,8 @@ typedef struct list{
     int sum;
 } List;
 
-void bubble(int values[], int size){
-    for(int i = 0; i < size; i++){    
-        for(int j = 0; j < size-1-i; j++){
-            int aux;
-            if(values[j] > values[j+1]){
-                aux = values[j];
-                values[j] = values[j+1];
-                values[j+1] = aux;
-            }
-        }
-    }
-}
+void bubble(int values[], int size);
+void bubbleList(List list[], int size);
 
 
 int main(){
@@ -38,6 +28,7 @@ int main(){
     while (fgets(line, sizeof(line), fp_in) != NULL){
         List lists[512];
         int count = -1;
+        char text[1000];
 
         char *slice = strtok(line, space); // Quebra inicial por espaços
 
@@ -62,24 +53,52 @@ int main(){
             bubble(lists[i].values, lists[i].size);
         }
 
-        // printar no console cada lista
+        // ordenar as listas por soma total
+        bubbleList(lists, count+1);
+        // escrever as listas no arquivo
+        // se duas listas tiverem a mesma soma, considerar apenas a ultima
+        if(lineCount!=0)sprintf(text, "\n");
+        else sprintf(text, "");
         for(int i = 0; i<=count; i++){
-            printf("line %d, list %d: ", lineCount+1, i+1);
-            for(int j = 0; j < lists[i].size; j++){
-                printf("%d ", lists[i].values[j]);
+            if(lists[i].sum != lists[i+1].sum){
+                if(i==0)sprintf(text, "%sstart", text);
+                else sprintf(text, "%s start", text);
+                for(int j = 0; j < lists[i].size; j++){
+                    sprintf(text, "%s %d", text, lists[i].values[j]);
+                }
             }
-            printf(" | sum: %d\n", lists[i].sum);
         }
-        
-        // liberar lista no final
-        // for(int i = 0; i <= count; i++){
-        //     free(lists[i].values);
-        // }
-        // free(lists);
+        fputs(text, fp_out);
         lineCount++;
     }
 
     fclose(fp_in);
     fclose(fp_out);
     return 0;
+}
+
+void bubble(int values[], int size){
+    for(int i = 0; i < size; i++){    
+        for(int j = 0; j < size-1-i; j++){
+            int aux;
+            if(values[j] > values[j+1]){
+                aux = values[j];
+                values[j] = values[j+1];
+                values[j+1] = aux;
+            }
+        }
+    }
+}
+
+void bubbleList(List list[], int size){
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size-1-i; j++){
+            List aux;
+            if(list[j].sum > list[j+1].sum){
+                aux = list[j];
+                list[j] = list[j+1];
+                list[j+1] = aux;
+            }
+        }
+    }
 }
