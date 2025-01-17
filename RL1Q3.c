d7b1fbd430f28c35fe15ae109f0d14e9205f288f
uKuroo <opitimalracer50@hotmail.com>
2025-01-16 22:59:52 -0300

    -- Ordenaçãozinha de cria nos LE

--- RL1Q3.c
+++ RL1Q3.c
@@ -6,23 +6,38 @@
 typedef struct LI
 {
     int num;
-    LI *prox;
+    struct LI *prox;
 }LI;
 
 typedef struct LE
 {
     int num;
-    LI *elementos;
-    LE *anterior;
-    LE *prox;
+    struct LI *elementos;
+    struct LE *anterior;
+    struct LE *prox;
 }LE;
 
+
+void OrdenarVetor(LE arr[], int qtdLE) {
+    int i, j, temp;
+
+    for (i = 0; i < qtdLE - 1; i++) {
+        for (j = 0; j < qtdLE - i - 1 && arr[j].prox != NULL; j++) {
+            if (arr[j].num > arr[j + 1].num) {
+                temp = arr[j].num;
+                arr[j].num = arr[j + 1].num;
+                arr[j + 1].num = temp;
+            }
+        }
+    }
+}
 
 int main(){
     FILE *fp_in = fopen("L1Q3.in", "r");
     FILE *fp_out = fopen("L1Q3.out", "w");
     char line[1000];
-    char space = " ";
+    char space[] = " ";
+    
     if (fp_in == NULL || fp_out == NULL){
         printf("Arquivos nao podem ser abertos.");
         return EXIT_FAILURE;
@@ -31,6 +46,7 @@ int main(){
     printf("TEST");
 
     while (fgets(line, sizeof(line), fp_in) != NULL){
+        int qtdLE = 0;
         LE Ancora[1000];
         Ancora[0].anterior = NULL;
 
@@ -38,20 +54,38 @@ int main(){
 
         char *slice = strtok(line, space);
 
+        //Adquirir todos os elementos da ancora após LE e antes de LI
         if(strcmp(slice, "LE") == 0){
             slice = strtok(NULL, space);
-            for(int i = 0; strcmp(slice, "LI") != 0; i++){
+            for(int i = 0;slice != NULL && strcmp(slice, "LI") != 0; i++){
                 if(i > 0){
                     Ancora[i-1].prox = &Ancora[i];
                     Ancora[i].anterior = &Ancora[i-1];
                 } 
                 Ancora[i].prox = NULL;
                 Ancora[i].num = atoi(slice);
+                qtdLE++;
+                slice = strtok(NULL, &space);
             }
         }
-        if(strcmp(slice, "LI")){
 
+        for(int i = 0; i < qtdLE;i++){
+            fprintf(fp_out,"%d ",Ancora[i].num);
         }
+        fprintf(fp_out," | ");
+        //ordenar lista LE
+        OrdenarVetor(Ancora, qtdLE);
+        printf("\n");
+
+        for(int i = 0; i < qtdLE;i++){
+            fprintf(fp_out,"%d ",Ancora[i].num);
+        }
+        fprintf(fp_out,"\n");
+
+        //Adquirir todos os elementos da lista circular após o LI
+        // if(strcmp(slice, "LI")){
+
+        // }
     }
 
     fclose(fp_in);
